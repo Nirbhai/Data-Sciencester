52b404625b517d4c8b92248b20a2fe79a3c4bf62
Nirbhai Singh <chittamor@gmail.com>
2022-01-15 16:36:22 +0530

    Algebra is a way of seeing and thinking

---
+++ linearAlgebra.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Is anything more useless or less useful than algebra?
+    - Billy connolly
+
+
+
+
+Author	: Nirbhai Singh
+E-Mail	: chittamor@gmail.com
+
+
+*********************************
+
+Run below commands to verify you are in correct environment and branch:
+    !conda info -e
+    !git branch -a
+
+*********************************
+
+
+"""
