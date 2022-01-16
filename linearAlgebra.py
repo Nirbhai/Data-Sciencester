dcfd0b34736879ef16c82b953f7dc96403aadc18
Nirbhai Singh <chittamor@gmail.com>
2022-01-17 05:09:25 +0530

    built basic vector & matrices arithmatic tools

--- linearAlgebra.py
+++ linearAlgebra.py
@@ -111,20 +111,158 @@ assert dot([1, 1, 1], [1, 1, 1]) == 3
 assert dot([1, 1], [0, 0]) == 0
 
 def sum_of_squares(v: Vector) -> float:
+    """
+    returns summation of element-wise squares
+    v_1*v_1.....v_n*v_n
+    """
+    return dot(v, v)
+
+assert sum_of_squares([1, 1, 1]) == 3
+assert sum_of_squares([1, 2, 3]) == 14
+
+import math
+def magnitude(v: Vector) -> float:
+    """returns magnitude (or length) of the vector"""
+    return math.sqrt(sum_of_squares(v))
+
+assert magnitude([3, 4]) == 5
+assert magnitude([1, 2, 3, 1, 3, 1]) == 5 
+
+def squared_distance(v: Vector, w: Vector) -> float:
+    """computes summation of (v_i - w_i)**2 for all elements"""
+    return sum_of_squares(subtract(v, w))
+
+def distance(v: Vector, w: Vector) -> float:
+    """computes distance between two vectors"""
+    return magnitude(subtract(v, w))
+
+assert distance([6, 8], [3, 4]) == 5
+
+### Matrices ###
+
+Matrix = List[List[float]]
+
+A = [[1, 2, 3],
+     [4, 5, 6]]     # A has 2 rows and 3 columns
+
+B = [[1, 2],
+     [3, 4],
+     [5, 6]]        # B has 3 rows and 2 columns
+
+from typing import Tuple
+
+def shape(A: Matrix) -> Tuple:
+    """
+    Parameters
+    ----------
+    A : Matrix
+
+    Returns
+    -------
+    Tuple
+        (# of rows in A, # of columns in A).
+    """
+    assert A, "empty matrix"
+    return (len(A), len(A[0]) if A else 0)
+
+assert shape([[]])
+assert shape([[1, 2], [3,4]]) == (2, 2)
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """
     
 
+    Parameters
+    ----------
+    A : Matrix
+    i : int
+        i-th row
 
+    Returns
+    -------
+    Vector
+        i-th row of Matrix A
 
+    """
+    return A[i]
 
+def get_column(A: Matrix, j: int) -> Vector:
+    """
+    
 
+    Parameters
+    ----------
+    A : Matrix
+    j : int
+        j-th column
 
+    Returns
+    -------
+    Vector
+        j-th column of Matrix A
 
+    """
+    return [ A_i[j] 
+            for A_i in A ]
 
+assert get_row(A, 0) == [1, 2, 3]
+assert get_column(B, 0) == [1, 3, 5]
 
+from typing import Callable
 
+def make_matrix(num_rows: int,
+                num_columns: int,
+                entry_fx: Callable[[int, int], float]) -> Matrix:
+    """
+    
 
+    Parameters
+    ----------
+    num_rows : int
+        number of rows in final Matrix.
+    num_columns : int
+        number of columns in final Matrix.
+    entry_fx : Callable[[int, int], float]
+        function of generate A[i][j] element of Matrix A, given i, j.
 
+    Returns
+    -------
+    Matrix
+        Matrix A whose element A[i][j] is entry_fx(i, j) .
 
+    """
+    return [[entry_fx(i, j)
+            for j in range(num_columns)]
+            for i in range(num_rows)
+            ]
+
+def identity_matrix(n: int) -> Matrix:
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(3) == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+print(identity_matrix(5))
+
+"""
+    Why Matrices are important:
+        1. A Matrix can be used to represent a dataset
+        consisting multiple vectors,
+        each row being a vector.
+        Example: 
+            if we had 1000 people with height, weight, age
+            we could put them in a 1000 x 3 matrix
+        
+        2. We can use n x k matrix to represent a linear function
+        that maps k-dimensional vectors to n-dimensional vectors
+        
+        3. Matrices can be used to represent binary relationships 
+        between pairs(i, j).
+        Example:
+            A[i][j] = 1 can represent presence of an edge
+            between node i and node j in a graph, else zero
+            to represent absence of an edge.
+        
+        Use numpy for vectors and matrices.
+"""
 
 
 
