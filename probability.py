0341f2db21c2405d81e6cad07007ac9e7dec5eb6
Nirbhai Singh <chittamor@gmail.com>
2022-01-17 16:11:22 +0530

    probability is a mouthful

---
+++ probability.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+For a data scientist it is beneficial to think of Probability 
+as a way of quantifying the uncertainty associated with events 
+chosen from some universe of events.
+Rather than getting technical about what these terms mean, 
+think of rolling a die. The universe consists of all possible outcomes. 
+And any subset of these outcomes is an event; 
+for example, “the die rolls a 1” or “the die rolls an even number.”
+    - Joel Grus
+
+
+Notationally, we write P(E) to mean “the probability of the event E”
+
+Author	: Nirbhai Singh
+E-Mail	: chittamor@gmail.com
+
+
+*********************************
+
+Run below commands to verify you are in correct environment and branch:
+    !conda info -e
+    !git branch -a
+
+*********************************
+
+
+"""
+
+"""
+    Roughly speaking, we say that two events E and F are dependent 
+    if knowing something about whether E happens 
+    gives us information about whether F happens (and vice versa). 
+    Otherwise, they are independent.
+    For instance, if we flip a fair coin twice, 
+    knowing whether the first flip is heads 
+    gives us no information about whether the second flip is heads. 
+    These events are independent. 
+    On the other hand, knowing whether the first flip is heads
+    certainly gives us information about whether both flips are tails.
+    These two are dependent events.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
