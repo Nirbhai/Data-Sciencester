5d103c2a63c20d108b22d70727054a0aecfe347b
Nirbhai Singh <chittamor@gmail.com>
2022-02-01 16:57:18 +0530

    just created the file for for working with data, will implement later

---
+++ workingData.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+
+Author	: Nirbhai Singh
+E-Mail	: chittamor@gmail.com
+
+
+*********************************
+
+Run below commands to verify you are in correct environment and branch:
+    !conda info -e
+    !git branch -a
+
+*********************************
+
+
+"""
+
+from typing import List, Dict
+from collections import Counter
+import math
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
