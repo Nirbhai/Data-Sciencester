0fa49d4eb8621582ad4d6a99bc1e6efc1e617fb8
Nirbhai Singh <chittamor@gmail.com>
2022-02-16 05:51:42 +0530

    ran into an import error while importing a local module

--- workingData.py
+++ workingData.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 
-
+Experts often possess more data than judgement.
+    - Colin Powell
 
 Author	: Nirbhai Singh
 E-Mail	: chittamor@gmail.com
@@ -24,7 +25,40 @@ from collections import Counter
 import math
 import matplotlib.pyplot as plt
 
+def bucketize(point: float, bucket_size: float) -> float:
+    """Floor the point to the next lower multiple of bucket_size"""
+    return bucket_size * math.floor(point / bucket_size)
 
+def make_histogram(points: List[float],
+                   bucket_size: float) -> Dict[float, int]:
+    """buckets the points and counts how many in each bucket"""
+    return Counter(bucketize(point, bucket_size) for point in points)
+
+def plot_histogram(points: List[float],
+                   bucket_size: float,
+                   title: str = "") -> None:
+    histogram = make_histogram(points, bucket_size)
+    plt.bar(histogram.keys(), histogram.values(), width=bucket_size)
+    plt.title(title)
+
+import random
+from probability import inverse_normal_cdf
+
+random.seed(0)
+
+# uniform between -100 and 100
+uniform = [200 * random.random() - 100 for _ in range(10000)]
+
+# normal distribution with mean = 0, std dev = 57
+normal = [57 * inverse_normal_cdf(random.random())
+          for _ in range(10000)]
+
+from statistics import mean
+
+print(f" mean of uniform data set: {mean(uniform)}")
+print(f" mean of normal data set: {mean(normal)}")
+
+from statistics import standard_deviation
 
 
 
